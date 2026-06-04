c892f68fbe889dab89ff309787b145da497ca926
SilasNS <silasschlax@gmail.com>
2026-06-04 16:56:48 -0400

    Add default suit value to make testing easier

--- lib/card.rb
+++ lib/card.rb
@@ -8,7 +8,7 @@ class Card
   RANKS = %w[2 3 4 5 6 7 8 9 10 J Q K A].freeze
   SUITS = %w[Spades Hearts Diamonds Clubs].freeze
 
-  def initialize(rank, suit)
+  def initialize(rank, suit = 'Spades')
     raise InvalidRank unless RANKS.include?(rank)
     raise InvalidSuit unless SUITS.include?(suit)
 
