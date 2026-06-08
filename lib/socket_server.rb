079419c3a66872e41c35dfceb352dba9d070c8c2
SilasNS <silasschlax@gmail.com>
2026-06-08 18:50:27 -0400

    Create run_round and helper methods

--- lib/socket_server.rb
+++ lib/socket_server.rb
@@ -1,11 +1,14 @@
 require 'socket'
-require_relative 'deck'
-require_relative 'player'
+require_relative 'go_fish_game'
 require_relative 'client'
+
+MIN_GAME_SIZE = 2
+MAX_GAME_SIZE = 6
+INPUT_SYMBOL = ' -> '.freeze
 
 # Socket Server Class
 class SocketServer
-  attr_accessor :server, :clients, :games
+  attr_accessor :server, :clients, :games, :game_size
 
   def port_number
     3336
@@ -15,19 +18,49 @@ class SocketServer
     @server = TCPServer.new(port_number)
   end
 
+  def games
+    @games ||= []
+  end
+
   def clients
     @clients ||= []
   end
 
   def accept_new_client
     client = @server.accept_nonblock
-    clients << Client.new(client)
+    clients << Client.new(client, host: host?, player_id: create_player_id)
     client.puts 'Welcome to Go Fish!'
   rescue IO::WaitReadable, Errno::EINTR
-    puts 'No Client to Accept...'
+    # puts 'No Client to Accept...'
   end
 
-  def read_socket(socket, delay = 0.7)
+  def create_game_if_possible
+    return unless clients.count == MIN_GAME_SIZE
+
+    clients.each do |client|
+      message_to_users = 'The Go Fish Game is Starting...'
+      write_socket(client.socket, message_to_users)
+    end
+
+    create_game
+  end
+
+  def run_game(game)
+    run_turn(game) until true == false
+  end
+
+  def run_turn(game)
+    current_player = current_player(game)
+    return false unless current_player.ask_for_player
+    return false unless current_player.ask_for_rank
+
+    game.run_turn(current_player.selected_player.to_i, current_player.selected_rank)
+    @clients.each do |client|
+      client.write_socket(game.results.for_current)
+    end
+  end
+
+  def read_socket(socket, delay = 1)
     sleep(delay)
     socket.read_nonblock(1000)
   rescue IO::WaitReadable
@@ -40,5 +73,43 @@ class SocketServer
 
   def stop
     @server&.close
+  end
+
+  private
+
+  def current_player(game)
+    current_player_turn = game.current_player.player_id
+    clients.select { |client| client.player_id == current_player_turn }.first
+  end
+
+  def create_game
+    game = GoFishGame.new(create_client_data_hash)
+    game.start
+    games << game
+    send_hands_to_players
+    game
+  end
+
+  def create_client_data_hash
+    data_hash = []
+    clients.map do |client|
+      data_hash << { name: client.name, player_id: client.player_id }
+    end
+    data_hash
+  end
+
+  def send_hands_to_players
+    clients.each do |client|
+      player = games.first.find_player(client.player_id)
+      client.write_socket(player.format_hand)
+    end
+  end
+
+  def host?
+    clients.empty?
+  end
+
+  def create_player_id
+    @clients.size + 1
   end
 end
--- spec/socket_server_spec.rb
+++ spec/socket_server_spec.rb
@@ -1,13 +1,14 @@
 require 'socket'
 require_relative '../lib/socket_server'
 require_relative 'helpers/mock_socket_client'
+require_relative '../lib/card'
 
 describe SocketServer do
   before(:each) do
     @clients = []
     @server = SocketServer.new
     @server.start
-    sleep 0.1 # Ensure server is ready for clients
+    sleep 0.1
   end
 
   after(:each) do
@@ -21,17 +22,98 @@ describe SocketServer do
     @server.stop
     expect { MockSocketClient.new(@server.port_number) }.to raise_error(Errno::ECONNREFUSED)
   end
-  it 'clients get a welcome message' do
-    client1 = MockSocketClient.new(@server.port_number)
-    @clients.push client1
-    @server.accept_new_client
-    expect(client1.capture_output).to match(/welcome/i)
+
+  describe '#accept_new_client' do
+    context 'when the first player joins' do
+      let(:client1) { MockSocketClient.new(@server.port_number) }
+      before do
+        @clients.push client1
+        @server.accept_new_client
+        sleep 0.3
+      end
+      it 'clients get a welcome message' do
+        expect(client1.capture_output).to match(/welcome/i)
+      end
+      it 'adds client to clients array' do
+        expected_clients = 1
+        expect(@server.clients.size).to eq expected_clients
+      end
+      it 'is defined as host' do
+        client = @server.clients.first
+        expect(client.host).to be true
+      end
+      it 'has an id of 0' do
+        client = @server.clients.first
+        expected_player_id = 1
+        expect(client.player_id).to eq expected_player_id
+      end
+    end
+  end
+  describe '#run_turn' do
+    let!(:client1) { create_test_client }
+    let!(:client2) { create_test_client }
+    before do
+      @server.create_game_if_possible
+    end
+    let!(:game) { @server.games.first }
+    it 'returns false if current player has not selected a player' do
+      expect(@server.run_turn(game)).to be false
+    end
+    it 'returns if current player has selected a player but not rank' do
+      client1.provide_input('0')
+      expect(@server.run_turn(game)).to be false
+    end
+    context 'when a turn is played' do
+      let(:game) { @server.games.first }
+      let(:player1) { game.players.first }
+      let(:player2) { game.players.last }
+      before do
+        player1.hand = [Card.new('K')]
+        player2.hand = [Card.new('Q')]
+        game.deck.cards = [Card.new('2')]
+      end
+      it 'starts a plays a turn' do
+        client1.provide_input('1')
+        @server.run_turn(game)
+        client1.provide_input('Q')
+        @server.run_turn(game)
+        expected_hand_size = 2
+        expect(player1.hand_size).to eq expected_hand_size
+      end
+    end
   end
 
+  describe '#create_game_if_possible' do
+    context 'when 1 player' do
+      let!(:client) { create_test_client }
+      it 'does not create game' do
+        @server.create_game_if_possible
+        expect(@server.games.count).to be_zero
+      end
+    end
+    context 'when 2 players' do
+      let!(:client1) { create_test_client }
+      let!(:client2) { create_test_client }
+      let(:regex) { /Hearts|Diamonds|Clubs|Spades/i }
+      it 'sends both players a message and creates game' do
+        @server.create_game_if_possible
+        expected_game_size = 1
+        expect(client1.capture_output).to match(/starting/i)
+        expect(client2.capture_output).to match(/starting/i)
+        expect(@server.games.count).to eq expected_game_size
+      end
+      it 'sends all users their hands' do
+        @server.create_game_if_possible
+        expect(client1.capture_output).to match regex
+        expect(client2.capture_output).to match regex
+      end
+    end
+  end
   describe '#read_socket' do
     let!(:client) { create_test_client }
     it 'gets message sent to server' do
-      @server.write_socket(@server.clients.first, 'message')
+      server_client = @server.clients.first
+      @server.write_socket(server_client.socket, 'message')
       expect(@server.read_socket(client.socket)).to match(/message/i)
     end
   end
@@ -39,8 +121,8 @@ describe SocketServer do
   describe '#write_socket' do
     let!(:client) { create_test_client }
     it 'sends a message to the socket' do
-      # binding.irb
-      @server.write_socket(@server.clients.first, 'test')
+      server_client = @server.clients.first
+      @server.write_socket(server_client.socket, 'test')
       expect(client.capture_output).to match(/test/i)
     end
   end
@@ -49,6 +131,7 @@ describe SocketServer do
     client = MockSocketClient.new(@server.port_number)
     @clients.push(client)
     @server.accept_new_client
+    sleep 0.1
     client.capture_output
     client
   end
